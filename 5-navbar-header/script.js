87b357d6f7bbabad3502b3d19f6b4868fb9331f9
Adriano Cruz <92281726+adrianocrhuz@users.noreply.github.com>
2025-03-28 20:47:30 +0000

    Projetos

---
+++ 5-navbar-header/script.js
@@ -0,0 +1,11 @@
+/*let navbar = document.querySelector(".navbar")
+let menu = document.querySelector("#menu-icon")
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle('open')
+}
+    */
+
+
+
